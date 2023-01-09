10aaca4aa3a7a2ff3d0ceb1b6e6ec69794050cd7
ElliotLuque <elliotluque@gmail.com>
2023-01-09 12:51:54 +0100

    fix prettier tailwind plugin, lint project

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,34 @@ import { useCopyToClipboard } from 'usehooks-ts'
 import { useLocalStorage } from 'usehooks-ts'
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid/'
 
+const generateRandomUserJson = () => {
+  const name = faker.name.firstName()
+  const lastname = faker.name.lastName()
+
+  const user: User = {
+    id: faker.datatype.uuid(),
+    name: name,
+    lastname: lastname,
+    avatar: faker.image.avatar(),
+    birthDate: faker.date.birthdate(),
+    email: faker.internet.email(name, lastname),
+    phoneNumber: faker.phone.number('+34 6## ### ###'),
+    favouriteMusicGenre: faker.music.genre(),
+    originCountry: faker.address.country(),
+    address: {
+      street: faker.address.streetAddress(),
+      zipcode: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+  }
+
+  return user
+}
+
 function App() {
   const [isDarkTheme, setDarkTheme] = useLocalStorage('dark', true)
+  const [randomUser, setRandomUser] = useState<User>(generateRandomUserJson())
   const [value, copy] = useCopyToClipboard()
-  const [randomUser, setRandomUser] = useState<User>()
-
-  const generateRandomUserJson = () => {
-    const name = faker.name.firstName()
-    const lastname = faker.name.lastName()
-
-    const user: User = {
-      id: faker.datatype.uuid(),
-      name: name,
-      lastname: lastname,
-      avatar: faker.image.avatar(),
-      birthDate: faker.date.birthdate(),
-      email: faker.internet.email(name, lastname),
-      phoneNumber: faker.phone.number('+34 6## ### ###'),
-      favouriteMusicGenre: faker.music.genre(),
-      originCountry: faker.address.country(),
-      address: {
-        street: faker.address.streetAddress(),
-        zipcode: faker.address.zipCode(),
-        country: faker.address.country(),
-      },
-    }
-
-    return user
-  }
 
   if (isDarkTheme) {
     document.documentElement.classList.add('dark')
@@ -42,36 +42,36 @@ function App() {
   }
 
   return (
-    <div className='flex flex-col justify-center w-screen h-screen bg-white dark:bg-gray-900'>
-      <header className='flex justify-end w-full pt-8 px-16 '>
+    <div className='flex h-screen w-screen flex-col justify-center bg-white dark:bg-gray-900'>
+      <header className='flex w-full justify-end px-16 pt-8'>
         {isDarkTheme ? (
           <SunIcon
-            className='h-10 w-10 text-white m-4 cursor-pointer'
+            className='m-4 h-10 w-10 cursor-pointer text-white'
             onClick={() => setDarkTheme(false)}
           />
         ) : (
           <MoonIcon
-            className='h-10 w-10 text-gray-700 m-4 cursor-pointer'
+            className='m-4 h-10 w-10 cursor-pointer text-gray-700'
             onClick={() => setDarkTheme(true)}
           />
         )}
       </header>
-      <main className='flex justify-center w-full h-full items-center'>
-        <div className='flex flex-col items-center justify-start md: gap-16 lg:gap-20 xl:gap-32'>
-          <h1 className='text-6xl text-gray-900 dark:text-white font-extrabold leading-none tracking-tight'>
+      <main className='flex h-full w-full items-center justify-center'>
+        <div className='md: flex flex-col items-center justify-start gap-16 lg:gap-20 xl:gap-32'>
+          <h1 className='text-6xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white'>
             User generator with Faker.js
           </h1>
           <div className='flex flex-col items-center gap-16'>
             <JsonView userData={randomUser as User} />
             <div className='flex items-center gap-8'>
               <button
-                className='py-4 px-5 mr-2 mb-2 text-md font-medium focus:outline-none rounded-lg border border-gray-300 text-gray-600 dark:border-gray-600 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-700'
+                className='text-md mr-2 mb-2 rounded-lg border border-gray-300 py-4 px-5 font-medium text-gray-600 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700'
                 onClick={() => setRandomUser(generateRandomUserJson())}
               >
                 Generate new user
               </button>
               <button
-                className='py-4 px-5 mr-2 mb-2 text-md font-medium focus:outline-none rounded-lg border border-green-600 focus:ring-green-700 bg-green-500 text-white hover:text-gray-300 hover:bg-green-700'
+                className='text-md mr-2 mb-2 rounded-lg border border-green-600 bg-green-500 py-4 px-5 font-medium text-white hover:bg-green-700 hover:text-gray-300 focus:outline-none focus:ring-green-700'
                 onClick={() => copy(JSON.stringify(randomUser))}
               >
                 Copy to clipboard
--- src/components/jsonView.tsx
+++ src/components/jsonView.tsx
@@ -1,27 +1,9 @@
 import React from 'react'
 import { User } from '../types/user'
 
-const JsonView: React.FC<{ userData: User }> = ({
-  userData = {
-    id: 'ecf6891a-7b08-4753-ba0f-37438184147c',
-    name: 'Ursula',
-    lastname: 'Reinger',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/950.jpg',
-    birthDate: '1975-07-21T10:46:35.377Z',
-    email: 'Ursula4@hotmail.com',
-    phoneNumber: '+34 629 307 328',
-    favouriteMusicGenre: 'Rock',
-    originCountry: 'Turks and Caicos Islands',
-    address: {
-      street: '6457 Cielo Mews',
-      zipcode: '91178-7182',
-      country: 'Haiti',
-    },
-  },
-}) => {
+const JsonView: React.FC<{ userData: User }> = ({ userData }) => {
   return (
-    <div className=' dark:bg-gray-800 p-10 text-gray-900 dark:text-gray-400 rounded-2xl font-medium dark:border-2 dark:border-gray-700 shadow-md dark:shadow-none border-[0.0875rem] border-gray-200'>
+    <div className='rounded-2xl border-[0.0875rem] border-gray-200 p-10 font-medium text-gray-900 shadow-md dark:border-2 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:shadow-none'>
       <pre className='text-lg'>{JSON.stringify(userData, null, 2)}</pre>
     </div>
   )
